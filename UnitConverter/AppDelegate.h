9ac692d87e7f18cda85f07234f9b2578541e5c59
Miguel Melendez <m.melend@gmail.com>
2016-05-16 19:58:44 -0400

    Initial Commit

---
+++ UnitConverter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UnitConverter
+//
+//  Created by Miguel Melendez on 5/16/16.
+//  Copyright © 2016 Miguel Melendez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
